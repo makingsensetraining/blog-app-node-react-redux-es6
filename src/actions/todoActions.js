caf8b894c2b6a7bab024efecd428b36e7b248c10
Juan Pablo Cook <juampick@gmail.com>
2016-11-09 18:22:37 -0300

    Added isomorphic-fetch library to allow usage of fetch method - real apis

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import todoAPI from '../api/mockTodoApi';
+import fetch from 'isomorphic-fetch';
 
 export function loadTodoSuccess(todos){
     return { type: types.LOAD_TODO_SUCCESS, todos };
@@ -14,7 +15,7 @@ export function updateCourseSuccess(todo){
 }
 
 export function loadTodos(){
-    return function(dispatch){
+    return dispatch => {
         return todoAPI.getAllTodos().then(todos => {
             dispatch(loadTodoSuccess(todos));
         }).catch(error => {
@@ -24,7 +25,7 @@ export function loadTodos(){
 }
 
 export function saveTodo(todo){
-    return function(dispatch, getState){
+    return (dispatch, getState) => {
         return todoAPI.saveTodo(todo).then(todoSaved => {
             todo.id ? dispatch(updateCourseSuccess(todoSaved)) :
                 dispatch(createTodoSuccess(todoSaved));
@@ -33,3 +34,36 @@ export function saveTodo(todo){
         });
     };
 }
+
+/*
+    For using REAL Api Calls
+    -> Example http call from original doc: http://redux.js.org/docs/advanced/AsyncActions.html
+    using thunk and fetch (needs polyfill)
+
+
+function fetchPosts(subreddit) {
+    return dispatch => {
+        dispatch(requestPosts(subreddit))
+        return fetch(`http://www.reddit.com/r/${subreddit}.json`)
+            .then(response => response.json())
+            .then(json => dispatch(receivePosts(subreddit, json)))
+    }
+}
+
+// Other example:
+ return fetch('http://<apiURL>')
+     .then(response => response.json())
+     .then(json => console.log(json));
+
+// Fetch using Post JSON:
+ fetch('/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    name: 'Hubot',
+    login: 'hubot',
+    })
+ })
+*/
