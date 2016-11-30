ca4b3b0bd11d115d92bf9373be90db2950667eb3
Juan Pablo Cook <juampick@gmail.com>
2016-11-30 13:02:21 -0300

    Added boron modal library and a custom Modal component and the implementation in a basic example

--- app/components/home/HomePage.js
+++ app/components/home/HomePage.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Modal from '../common/Modal';
 
 class HomePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.openModal = this.openModal.bind(this);
+    }
+
+    openModal(){
+        this.modal.open();
+    }
+
     render(){
         return (
             <div className="jumbotron">
                 <h1>Blog App</h1>
                 <p>Node, React, Redux and React Router in ES6 for ultra-respositve webapps.</p>
                 <Link to="/app/blog" className="btn btn-primary btn-lg">Check the latests posts</Link>
+
+                &nbsp;
+                <a href="#" className="btn btn-info btn-lg pull-right" onClick={this.openModal}>Info</a>
+                <Modal
+                    title="Modal Test"
+                    body="This is a testing modal example using bootstrap"
+                    ref={(child) => { this.modal = child; }}
+                />
             </div>
         );
     }
